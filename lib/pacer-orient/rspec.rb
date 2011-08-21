b1f21b1aa829c690c3ae9b208015b7718a18dceb
Darrick Wiebe <darrick@innatesoftware.com>
2011-08-21 01:20:17 -0400

    Run orient specs using in memory graph

--- lib/pacer-orient/rspec.rb
+++ lib/pacer-orient/rspec.rb
@@ -13,26 +13,17 @@ class Rspec::GraphRunner
 
     def orient_graph
       return @orient_graph if @orient_graph
-      path1 = File.expand_path('tmp/spec.orient')
-      dir = Pathname.new(path1)
-      dir.rmtree if dir.exist?
-      @orient_graph = Pacer.orient("local:#{path1}")
+      @orient_graph = Pacer.orient("memory:spec1")
     end
 
     def orient_graph2
       return @orient_graph2 if @orient_graph2
-      path2 = File.expand_path('tmp/spec.orient.2')
-      dir = Pathname.new(path2)
-      dir.rmtree if dir.exist?
-      @orient_graph2 = Pacer.orient("local:#{path2}")
+      @orient_graph2 = Pacer.orient("memory:spec2")
     end
 
     def orient_graph_no_indices
       return @orient_graph_no_indices if @orient_graph_no_indices
-      path3 = File.expand_path('tmp/spec_no_indices.orient')
-      dir = Pathname.new(path3)
-      dir.rmtree if dir.exist?
-      @orient_graph_no_indices = Pacer.orient("local:#{path3}")
+      @orient_graph_no_indices = Pacer.orient("memory:spec-no-idx")
       @orient_graph_no_indices.drop_index :vertices
       @orient_graph_no_indices.drop_index :edges
       @orient_graph_no_indices
