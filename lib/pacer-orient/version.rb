a4e237bd5edec74e28b3f4408c8ac037fd7e0a68
Darrick Wiebe <darrick@innatesoftware.com>
2011-08-24 01:42:26 -0400

    Version bumps will no longer require editing pom.xml I hope

--- lib/pacer-orient/version.rb
+++ lib/pacer-orient/version.rb
@@ -3,5 +3,7 @@ module Pacer
     VERSION = "1.0.3"
     JAR = "pacer-orient-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
+    BLUEPRINTS_VERSION = "1.0-SNAPSHOT"
+    PIPES_VERSION = "0.8-SNAPSHOT"
   end
 end
