d60a974272d0cc086559417bce233226aaade2dc
Darrick Wiebe <darrick@innatesoftware.com>
2012-09-04 15:56:16 -0400

    Release

--- lib/pacer-orient/version.rb
+++ lib/pacer-orient/version.rb
@@ -1,9 +1,10 @@
 module Pacer
   module Orient
-    VERSION = "2.0.0.pre"
+    VERSION = "2.0.0"
     JAR = "pacer-orient-#{ VERSION }-standalone.jar"
     JAR_PATH = "lib/#{ JAR }"
     BLUEPRINTS_VERSION = "2.1.0"
     PIPES_VERSION = "2.1.0"
+    PACER_REQ = ">= 1.0.0"
   end
 end
